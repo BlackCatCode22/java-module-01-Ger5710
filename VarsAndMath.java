2d648abbbdc5f5239d2181a540ac9a5f4c530d87
Ger5710 <Ger5710@icloud.com>
2025-01-23 20:11:36 -0800

    VarsAndMath

---
+++ VarsAndMath.java
@@ -0,0 +1,10 @@
+public class VarsAndMath {
+    public static void main(String[] args) {
+    int num1 = 11;
+        int num2 = 6;
+                String message = "the sum is: ";
+                int sum = num1 + num2;
+
+                System.out.println(message + sum);
+    }
+}
