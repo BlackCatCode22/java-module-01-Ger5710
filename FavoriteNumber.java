b703902c058a948a8fbb692857993537b4a9a7e0
Ger5710 <Ger5710@icloud.com>
2025-01-23 20:14:34 -0800

    FavoriteNumber

---
+++ FavoriteNumber.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class FavoriteNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your favorite number: ");
+        int favoriteNumber = scanner.nextInt();
+
+        System.out.println("Your favorite number is " + favoriteNumber + "!");
+
+        scanner.close();
+    }
+}
